d3ceefe76a313bab0d606efb5dfa7bcd86c623d7
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-09-25 09:48:46 +0200

    Add Comentary on ITask

--- Assets/Scripts/AIFrameWork/Interfaces/INode.cs
+++ Assets/Scripts/AIFrameWork/Interfaces/INode.cs
@@ -3,6 +3,12 @@ namespace AIFrameWork.Interfaces
     public interface INode
     {
         public ICondition Condition();
+        
+        /// <summary>
+        /// Execute a task. If the task takes more than one frame to execute, the three execution has to be blocked.
+        /// </summary>
+        /// <param name="blockable">Block the execution of a Tree if the task takes more than one frame to execute</param>
+        /// <returns></returns>
         public ITask Task(IBlockable blockable);
 
         public INode Succeeded();
