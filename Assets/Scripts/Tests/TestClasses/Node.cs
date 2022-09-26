5d06cd1b99fe210507feb536f165dbef1bc06e24
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-09-26 11:45:55 +0200

    Add Node

---
+++ Assets/Scripts/Tests/TestClasses/Node.cs
@@ -0,0 +1,26 @@
+﻿using AIFrameWork.Interfaces;
+
+namespace Tests.TestClasses
+{
+    public class Node : INode
+    {
+        private ICondition _condition;
+        private ITask _task;
+        private INode _succeeded;
+        private INode _failed;
+
+        public Node(ICondition condition, ITask task, INode succeeded, INode failed)
+        {
+            _condition = condition;
+            _task = task;
+            _succeeded = succeeded;
+            _failed = failed;
+        }
+
+        public ICondition Condition() => _condition;
+        public ITask Task() => _task;
+        public INode Succeeded() => _succeeded;
+        public INode Failed() => _failed;
+        
+    }
+}
